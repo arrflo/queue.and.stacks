ba534375f36535d747599160e3cac435b94f38b4
romalynfloranda <romalynfloranda0203@gmail.com>
2022-12-11 18:08:19 +0800

    for building stack data type

--- queues.py
+++ queues.py
@@ -1,3 +1,5 @@
+# for queue data type
+
 from collections import deque
 
 class Queue:
@@ -10,16 +12,52 @@ class Queue:
     def dequeue(self):
         return self._elements.popleft()
 
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
 from queues import Queue
+fifo =  Queue("1st", "2nd", "3rd")
+len(fifo)
 
-fifo = Queue()
-fifo.enqueue("1st")
-fifo.enqueue("2nd")
-fifo.enqueue("3rd")
+for element in fifo:
+    print (element)
 
-fifo.dequeue()
-'1st'
-fifo.dequeue()
-'2nd'
-fifo.dequeue()
-'3rd'
+len(fifo)
+
+#for building stack data type
+
+class Stack(Queue):
+    def dequeue (self):
+        return self._elements.pop()
+
+from queues import Stack
+
+lifo = Stack("1st","2nd", "3rd")
+for element in lifo:
+    print (element)
+
+
+lifo = []
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+lifo.pop()
+lifo.pop()
+lifo.pop()
+
