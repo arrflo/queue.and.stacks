dd8de98126ad629ddbb99148f0ed7901448e6412
romalynfloranda <romalynfloranda0203@gmail.com>
2022-12-13 00:09:44 +0800

    fpr building a priority queue data type

--- queues.py
+++ queues.py
@@ -75,4 +75,12 @@ from heapq import heappop
 heappop(fruits)
 fruits
 
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+person1 < person2
+person2 < person3
+
+#for building a priority queue data type
 
