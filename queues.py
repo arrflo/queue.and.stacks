f3454f60458e31e127c830720b49b18d1c77f18f
romalynfloranda <romalynfloranda0203@gmail.com>
2022-12-13 00:44:01 +0800

    for handling corner cases in your priority queue

--- queues.py
+++ queues.py
@@ -84,3 +84,49 @@ person2 < person3
 
 #for building a priority queue data type
 
+from collections import deque
+from heapq import heappop, heappush
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self,priority,value):
+        heappush(self._elements, (priority,value))
+
+    def dequeue (self):
+        return heappop(self._elements)
+
+from queues import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT,"Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL,"Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL,"Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT,"Hazard lights turned on")
+
+messages.dequeue()
+
+from collections import deque
+from heapq import heappop, heappush
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self,priority,value):
+        heappush(self._elements, (-priority,value))
+
+    def dequeue (self):
+        return heappop(self._elements)[1]
+
+messages.dequeue()
+messages.dequeue()
+messages.dequeue()
+messages.dequeue()
+
+#for handling corner cases in your priority queue
