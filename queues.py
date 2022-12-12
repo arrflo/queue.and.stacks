fc494f5ab0b4d50814b34cf2002d3ab1280c967f
romalynfloranda <romalynfloranda0203@gmail.com>
2022-12-13 01:07:45 +0800

    for refactoring the code using a mixin class

--- queues.py
+++ queues.py
@@ -130,3 +130,20 @@ messages.dequeue()
 messages.dequeue()
 
 #for handling corner cases in your priority queue
+
+from dataclasses import dataclass
+
+@dataclass
+class Message:
+    event: str
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
+
+wipers < hazard_lights
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
+
+messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
